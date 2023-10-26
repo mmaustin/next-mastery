b4a002c2139811ea5eb211bb05f188c1dd1164e8
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-10-26 08:47:44 -0400

    added placeholder profile pic and signout, create post links to navbar

--- project/components/Nav.jsx
+++ project/components/Nav.jsx
@@ -6,6 +6,9 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
+
+  const isUserLoggedIn = true;
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -16,8 +19,32 @@ const Nav = () => {
           height={30}
           className="object-contain"
         />
-        <p className="logo_text">Boss Promts</p>
+        <p className="logo_text">Boss Prompts</p>
       </Link>
+
+      <div className="sm:flex hidden ">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+            <button type="button" onClick={signOut} className="outline_btn">Sign Out</button>
+            <Link href="/profile">
+              <Image
+                src="/assets3/images/logo.svg"
+                width={37}
+                height={37}
+                className="rounded-full"
+                alt="profile"
+              />
+            </Link>
+          </div>
+        ) : (
+          <>
+
+          </>
+        )}
+      </div>
     </nav>
   )
 }
