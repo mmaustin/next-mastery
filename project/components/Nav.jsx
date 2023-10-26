079e019ee9ad25b9824e80ef165ad1632c2947bd
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-10-26 08:07:50 -0400

    added navbar image link to homepage

--- project/components/Nav.jsx
+++ project/components/Nav.jsx
@@ -1,9 +1,24 @@
+"use client"
 
-
+import Image from "next/image";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="/assets3/images/logo.svg"
+          alt="Boss Prompts Logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+        <p className="logo_text">Boss Promts</p>
+      </Link>
+    </nav>
   )
 }
 export default Nav
