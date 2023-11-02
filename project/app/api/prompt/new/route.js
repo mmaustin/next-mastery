1c5754aebabf99a9623160e015e3a6c531d0eeb4
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-11-02 13:22:17 -0400

    imported prompt model to the prompt create api and conncted to mongodb

--- project/app/api/prompt/new/route.js
+++ project/app/api/prompt/new/route.js
@@ -1,12 +1,21 @@
+import Prompt from "@models/prompt";
 import { connectToDB } from "@utils/database";
+
 
 export const promptPost = async req => {
   const { userId, prompt, tag } = req.json();
 
   try {
     await connectToDB();
+    const newPrompt = new Prompt({
+      creator: userId,
+      prompt,
+      tag
+    })
+    await newPrompt.save();
+    return Response(JSON.stringify(newPrompt), { status: 201 })
   } catch (error) {
-    console.log(error);
+    return new Response('Failed to create a new prompt', { status: 500 })
   }
 }
 
