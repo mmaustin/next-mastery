3e6f062059efcd169ebabf643995ce517fa6e344
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-11-08 08:32:42 -0500

    created shell functions edit and delete functions in my profile component

--- project/app/profile/page.jsx
+++ project/app/profile/page.jsx
@@ -1,9 +1,41 @@
-"use client"
+"use client";
+
+import Profile from "@components/Profile";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 
-const Profile = () => {
+const MyProfile = () => {
+
+  const { data: session } = useSession();
+
+  // useEffect(() => {
+  //   const fetchPosts = async () => {
+  //     const response = await fetch(`/api/users/${session?.user.id}/posts`);
+  //     const data = await response.json();
+
+  //     setPosts(data);
+  //   }
+  //   fetchPosts();
+  // }, []);
+
+  const handleEdit = () => {
+
+  };
+
+  const handleDelete = async () => {
+
+  };
+
   return (
-    <div>Profile</div>
+    <Profile
+      name="My"
+      desc="Welcome to your personalized page"
+      data={[]}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
   )
 }
-export default Profile
+export default MyProfile;
